8e9b91dbd24b07f5f85950698a1ebd7ae8b46b99
Psor1107 <psor1107@gmail.com>
2024-05-05 16:58:59 -0300

    pipe function

--- shell/main.c
+++ shell/main.c
@@ -7,37 +7,30 @@
 #include <fcntl.h>
 
 void executeCommand(char *command, char **arguments, int background);
-void sigHandler (int signum);
+void sigHandler(int signum);
+void executePipe(char **command1, char **command2, int background);
 
 int main()
 {
-
     char command[50];
     char *arguments[10];
     int background = 0;
     char *token;
-
     while (1)
     {
-
-        printf("$ ");
         fflush(stdout);
-
+        printf("$ ");
         fgets(command, sizeof(command), stdin);
-
         //checa input vazio
         if (strlen(command) != 1)
         {
             command[strcspn(command, "\n")] = '\0';
         }
-
-        token = strtok(command, " ");
-
-        if (strcmp(token, "exit") == 0)
+        if (strstr(command, "exit") != NULL)
         {
-            break;
+            return 0;
         }
-
+        token = strtok(command, " ");
         int i = 0;
         //itera pelos tokens e guarda os endereços em arguments[]
         while (token != NULL)
@@ -48,7 +41,6 @@ int main()
             i++;
         }
         arguments[i] = NULL;
-
         //checa se o programa deve rodar em background
         if (i > 0 && strcmp(arguments[i - 1], "&") == 0)
         {
@@ -66,35 +58,32 @@ int main()
 
 void executeCommand(char *command, char **arguments, int background)
 {
-
     pid_t pid = fork();
     int i = 0;
     int status;
-
     //checa se o fork falhou
     if (pid < 0)
     {
-        perror("fork failed");
+        perror("fork error");
         return;
     }
     else if (pid == 0) //filho
     {
-
         //checa se existe o operador ">"
         int fd = -1;
         for (i = 0; arguments[i] != NULL; i++)
         {
             if (strcmp(arguments[i], ">") == 0)
             {
-                fd = open(arguments[i+1], O_WRONLY| O_CREAT| O_TRUNC, S_IWUSR | S_IRUSR);
+                fd = open(arguments[i + 1], O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR | S_IRUSR);
                 if (fd == -1)
                 {
                     perror("open failed");
                     exit(1);
                 }
-                
+
                 dup2(fd, STDOUT_FILENO);
-                
+
                 //tira ">" e o nome do arquivo
                 arguments[i] = NULL;
                 arguments[i + 1] = NULL;
@@ -102,16 +91,52 @@ void executeCommand(char *command, char **arguments, int background)
                 break;
             }
         }
-        
-        if (execvp(command, arguments) < 0)
+
+        // checa se existe o operador "|"
+        int pipeIndex = -1;
+        for (i = 0; arguments[i] != NULL; i++)
         {
-            perror("error");
-            exit(1);
+            if (strcmp(arguments[i], "|") == 0)
+            {
+            pipeIndex = i;
+            break;
+            }
+        }
+
+        int j = 0;
+        int k = 0;
+
+        if (pipeIndex != -1)
+        {
+            char *command1[10];
+            char *command2[10];
+
+            //printf("command index 1 vale: %d\n command index 2 vale: %d\n", pipeIndex + 1, i - pipeIndex);
+
+            for (j = 0; j < pipeIndex; j++)
+            {
+            command1[j] = arguments[j];
+            }
+            command1[pipeIndex] = NULL;
+
+            for(j = pipeIndex + 1, k = 0; arguments[j] != NULL; j++, k++){
+                command2[k] = arguments[j];
+            }
+            command2[k] = NULL;
+            executePipe(command1, command2, background);
+        }
+        else
+        {
+            if (execvp(command, arguments) < 0)
+            {
+                perror("exec pipe S");
+                exit(1);
+            }
         }
     }
-    else    //pai
+    else //pai
     {
-        //wait() em caso de execucao em fg  
+        //wait() em caso de execucao em fg
         if (!background)
         {
             wait(NULL);
@@ -121,10 +146,77 @@ void executeCommand(char *command, char **arguments, int background)
     }
 }
 
-void sigHandler (int signum)
+void sigHandler(int signum)
 {
     pid_t pid;
     int status;
     pid = waitpid(0, &status, 0);
     //printf("Child %d terminated: %d %d\n",pid,signum,status);
+}
+
+void executePipe(char **command1, char **command2, int background)
+{
+    int pipefd[2];
+    pid_t pid1, pid2;
+
+    if (pipe(pipefd) == -1)
+    {
+        perror("pipe error");
+        exit(1);
+    }
+
+    pid1 = fork();
+    if (pid1 < 0)
+    {
+        perror("fork error");
+        exit(1);
+    }
+    else if (pid1 == 0)
+    {
+        close(pipefd[0]);
+
+        dup2(pipefd[1], STDOUT_FILENO);
+
+        if (execvp(command1[0], command1) < 0)
+        {
+            perror("exec pipe C1 error");
+            exit(1);
+        }
+    }
+    else
+    {
+        pid2 = fork();
+        if (pid2 < 0)
+        {
+            perror("fork error");
+            exit(1);
+        }
+        else if (pid2 == 0)
+        {
+            close(pipefd[1]);
+
+            dup2(pipefd[0], STDIN_FILENO);
+
+            if (execvp(command2[0], command2) < 0)
+            {
+                perror("exec pipe C2 error");
+                exit(1);
+            }
+        }
+        else
+        {
+            close(pipefd[0]);
+            close(pipefd[1]);
+
+            if (!background)
+            {
+                waitpid(pid1, NULL, 0);
+                waitpid(pid2, NULL, 0);
+            }
+            else
+            {
+                signal(SIGCHLD, sigHandler);
+            }
+        }
+    }
 }
